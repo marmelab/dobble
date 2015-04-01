c4f5817eb5ab7f3305ede5eb1110e7638043d241
djhi <gildas.garcia@gmail.com>
2015-04-01 17:06:09 +0200

    Fixes after review

--- test/card.js
+++ test/card.js
@@ -1,43 +1,41 @@
 var assert = require("assert");
 var Card = require("../lib/card");
 
-describe('Card', function(){
-  it('should throw an error when instanciated with no values', function(){
-    assert.throws(f => {
-      var card = new Card();
-    },
-    /You must specify the card values/);
-  });
-
-  it('can be instanciated with an array of values', function(){
-    var card = new Card(['a', 'b', 'c'])
-    assert.equal(3, card._symbols.length);
-  });
-
-  it('can be instanciated with multiple values as arguments', function(){
-    var card = new Card('a', 'b', 'c')
-    assert.equal(3, card._symbols.length);
-  });
-
-  describe('#validate()', function(){
-    it('should return true when initialized with two symbols and it contains only unique symbols', function(){
-      var card = new Card("A", "B");
-      assert.equal(true, card.validate());
+describe('Card', () => {
+  describe('constructor()', () => {
+    it('should throw an error when instanciated with no values', () => {
+      assert.throws(() => new Card());
     });
 
-    it('should return false when initialized with two symbols and it contains identical symbols', function(){
-      var card = new Card("A", "A");
-      assert.equal(false, card.validate());
+    it('does not accept anything that is not an array', () => {
+      assert.throws(() => new Card(42));
     });
 
-    it('should return true when initialized with many symbols and it contains only unique symbols', function(){
-      var card = new Card("A", "B", "C", "D", "E", "F", "G", "H", "I");
-      assert.equal(true, card.validate());
+    it('accepts an array of values', () => {
+      var card = new Card(['a', 'b', 'c']);
+      assert.equal(3, card.symbols.length);
+    });
+  });
+
+  describe('isValid()', () => {
+    it('should return true when initialized with two symbols and it contains only unique symbols', () => {
+      var card = new Card(["A", "B"]);
+      assert.ok(card.isValid());
     });
 
-    it('should return false when initialized with many symbols and it contains identical symbols', function(){
-      var card = new Card("B", "C", "A", "D", "A", "F", "G", "H", "A");
-      assert.equal(false, card.validate());
+    it('should return false when initialized with two symbols and it contains identical symbols', () => {
+      var card = new Card(["A", "A"]);
+      assert.ok(!card.isValid());
+    });
+
+    it('should return true when initialized with many symbols and it contains only unique symbols', () => {
+      var card = new Card(["A", "B", "C", "D", "E", "F", "G", "H", "I"]);
+      assert.ok(card.isValid());
+    });
+
+    it('should return false when initialized with many symbols and it contains identical symbols', () => {
+      var card = new Card(["B", "C", "A", "D", "A", "F", "G", "H", "A"]);
+      assert.ok(!card.isValid());
     });
   });
 });
--- test/pair.js
+++ test/pair.js
@@ -2,59 +2,49 @@ var assert = require("assert");
 var Card = require("../lib/card");
 var Pair = require("../lib/pair");
 
-describe('Pair', function(){
-  it('should throw an error when instanciated with no arguments', function(){
-    assert.throws(f => {
-      new Pair();
-    },
-    /Pair must be instantiated with two instances of Card/);
-  });
-
-  it('should throw an error when instanciated with only one argument', function(){
-    assert.throws(f => {
-      new Pair(new Card(1,2));
-    },
-    /Pair must be instantiated with two instances of Card/);
-  });
-
-  it('should throw an error when instanciated with non Card arguments', function(){
-    assert.throws(f => {
-      new Pair(1, 2);
-    },
-    /Pair must be instantiated with two instances of Card/);
-  });
-
-
-  describe('#validate()', function(){
-
-    it('should return false when any card is not valid', function(){
-      var card1 = new Card("A", "B", "C", "D", "E", "F", "G", "H", "I");
-      var card2 = new Card("A", "A", "K", "L", "M", "N", "O", "P", "Q");
-      assert.equal(true, card1.validate());
-      assert.equal(false, card2.validate());
-      var pair = new Pair(card1, card2);
-      assert.equal(false, pair.validate());
+describe('Pair', () => {
+  describe('constructor()', () => {
+    it('should throw an error when instanciated with no arguments', () => {
+      assert.throws(() => new Pair());
     });
 
-    it('should return true when cards contains only one identical symbol', function(){
-      var card1 = new Card("A", "B", "C", "D", "E", "F", "G", "H", "I");
-      var card2 = new Card("Z", "J", "K", "L", "M", "N", "O", "P", "A");
-      var pair = new Pair(card1, card2);
-      assert.equal(true, pair.validate());
+    it('should throw an error when instanciated with only one argument', () => {
+      assert.throws(() => new Pair(new Card([1,2])));
     });
 
-    it('should return false when cards contains more than one identical symbol', function(){
-      var card1 = new Card("A", "B", "C", "D", "E", "F", "G", "H", "I");
-      var card2 = new Card("A", "B", "K", "L", "M", "N", "O", "P", "Q");
+    it('should throw an error when instanciated with non Card arguments', () => {
+      assert.throws(() => new Pair(1, 2));
+    });
+  });
+
+  describe('isValid()', () => {
+
+    it('should return false when any card is not valid', () => {
+      var card1 = new Card(["A", "B", "C", "D", "E", "F", "G", "H", "I"]);
+      var card2 = new Card(["A", "A", "K", "L", "M", "N", "O", "P", "Q"]);
       var pair = new Pair(card1, card2);
-      assert.equal(false, pair.validate());
+      assert.ok(!pair.isValid());
     });
 
-    it('should return false when cards does not contains any identical symbol', function(){
-      var card1 = new Card("A", "B", "C", "D", "E", "F", "G", "H", "I");
-      var card2 = new Card("J", "K", "L", "M", "N", "O", "P", "Q", "R");
+    it('should return true when cards contains only one identical symbol', () => {
+      var card1 = new Card(["A", "B", "C", "D", "E", "F", "G", "H", "I"]);
+      var card2 = new Card(["Z", "J", "K", "L", "M", "N", "O", "P", "A"]);
       var pair = new Pair(card1, card2);
-      assert.equal(false, pair.validate());
+      assert.ok(pair.isValid());
+    });
+
+    it('should return false when cards contains more than one identical symbol', () => {
+      var card1 = new Card(["A", "B", "C", "D", "E", "F", "G", "H", "I"]);
+      var card2 = new Card(["A", "B", "K", "L", "M", "N", "O", "P", "Q"]);
+      var pair = new Pair(card1, card2);
+      assert.ok(!pair.isValid());
+    });
+
+    it('should return false when cards does not contains any identical symbol', () => {
+      var card1 = new Card(["A", "B", "C", "D", "E", "F", "G", "H", "I"]);
+      var card2 = new Card(["J", "K", "L", "M", "N", "O", "P", "Q", "R"]);
+      var pair = new Pair(card1, card2);
+      assert.ok(!pair.isValid());
     });
   });
 });
